de7aa100a1bf5f694a6adb54e9b348eafa6b1459
lxmhaha <1425558196@qq.com>
2017-08-18 11:58:37 +0800

    ooo

---
+++ js/index.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Administrator on 2017/7/15 0015.
+ */
+
+
+
+$(function(){
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'vertical',
+        loop: false,
+        onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiper); //隐藏动画元素
+            swiperAnimate(swiper); //初始化完成开始动画
+        },
+        onSlideChangeEnd: function(swiper){
+            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        }
+
+
+    });
+
+    $.fn.extend({
+        animateCss: function (animationName) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            this.addClass('animated ' + animationName).one(animationEnd, function() {
+                $(this).removeClass('animated ' + animationName);
+            });
+            return this;
+        }
+    });
+    // $('.lightxg').animateCss('fadeIn');
+
+
+});
+
+
